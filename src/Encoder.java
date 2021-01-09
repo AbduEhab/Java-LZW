338d243fdd33da6a31df61c41da1401586cabf97
AbduEhab <abdu.ehab0@gmail.com>
2021-01-09 15:24:35 +0200

    Fixed the encoder.

--- src/Encoder.java
+++ src/Encoder.java
@@ -2,65 +2,197 @@ import java.util.HashMap;
 
 public class Encoder {
 
-    public static String encode(String encodingSequence) {
+    // public static String encodeBinary(String encodingSequence) {
+
+    // HashMap<String, String> directory = new HashMap<String, String>();
+    // String buffer = "";
+    // String output = "";
+    // int storedValues = 0;
+
+    // boolean repeatedSequence = false;
+
+    // String[] byteString = new String[encodingSequence.length() / 8];
+
+    // for (int i = 0; i < byteString.length; i++) {
+    // byteString[i] = "";
+    // }
+
+    // for (int i = 0, j = 0; i < encodingSequence.length(); i++) {
+    // if (i != 0 && i % 8 == 0)
+    // j++;
+
+    // byteString[j] += encodingSequence.charAt(i) + "";
+    // }
+
+    // for (int i = 0; i < encodingSequence.length() / 8; i++) {
+    // buffer += byteString[i];
+
+    // if (i == 0) {
+    // directory.put(buffer, Integer.toBinaryString(storedValues++));
+    // // output += directory.get(Integer.toBinaryString(storedValues - 1));
+    // output += buffer;
+    // buffer = "";
+    // continue;
+    // }
+
+    // if (i == (encodingSequence.length() / 8) - 1) {
+    // if (directory.containsKey(buffer)) {
+    // output += directory.get(buffer);
+    // } else {
+    // if (buffer.length() != 8)
+    // output += directory.get(buffer.substring(0, buffer.length() - 8))
+    // + buffer.substring(buffer.length() - 9, buffer.length() - 1);
+    // else
+    // output += buffer;
+    // }
+    // break;
+    // }
+
+    // if (repeatedSequence) {
+    // if (directory.containsKey(buffer)) {
+    // continue;
+    // } else {
+    // directory.put(buffer, Integer.toBinaryString(storedValues++));
+    // // output += directory.get(Integer.toBinaryString(storedValues - 1));
+    // output += directory.get(buffer.substring(0, buffer.length() - 8))
+    // + buffer.substring(buffer.length() - 9, buffer.length() - 1);
+    // buffer = "";
+    // repeatedSequence = false;
+    // }
+    // } else {
+    // if (directory.containsKey(buffer)) {
+    // repeatedSequence = true;
+    // continue;
+    // } else {
+
+    // // if (buffer.equals(Integer.toBinaryString(storedValues++)))
+
+    // directory.put(buffer, Integer.toBinaryString(storedValues++));
+    // // output += directory.get(Integer.toBinaryString(storedValues - 1));
+    // output += buffer;
+    // buffer = "";
+    // }
+    // }
+
+    // }
+    // return output;
+    // }
+
+    public static String encodeBinary(String encodingSequence) {
+
+        // String temp = "";
+
+        // for (int i = 0; i < encodingSequence.length(); i++) {
+        // if (encodingSequence.charAt(i) != ' ')
+        // temp += "" + encodingSequence.charAt(i);
+        // }
+
+        // encodingSequence = temp;
 
         HashMap<String, String> directory = new HashMap<String, String>();
         String buffer = "";
-        int currIndex = 0;
         String output = "";
-        int storedValues = 0;
+        int storedValues = 256, basevalue = 256;
+
+        int inputBits = encodingSequence.length();
+        String extraBits = "0";
+
+        if (inputBits > 2048) {
+            storedValues = storedValues * 2;
+            basevalue = storedValues;
+            extraBits += "0";
+        }
+        if (inputBits > 4096) {
+            storedValues = storedValues * 2;
+            basevalue = storedValues;
+            extraBits += "0";
+        }
 
         boolean repeatedSequence = false;
 
-        for (int i = currIndex; i < encodingSequence.length(); i++) {
-            buffer += encodingSequence.charAt(i);
+        String[] byteString = new String[encodingSequence.length() / 8];
 
-            if (i == 0) {
-                directory.put(buffer, Integer.toBinaryString(storedValues++));
-                // output += directory.get(Integer.toBinaryString(storedValues - 1));
-                output += buffer;
-                buffer = "";
-                continue;
-            }
+        for (int i = 0; i < byteString.length; i++) {
+            byteString[i] = "";
+        }
 
-            if (i == encodingSequence.length() - 1) {
-                if (directory.containsKey(buffer)) {
-                    output += directory.get(buffer);
-                } else {
-                    output += directory.get(buffer.substring(0, buffer.length() - 1))
-                            + (buffer.charAt(buffer.length() - 1) + "");
+        for (int i = 0, j = 0; i < encodingSequence.length(); i++) {
+            if (i != 0 && i % 8 == 0)
+                j++;
+
+            byteString[j] += encodingSequence.charAt(i) + "";
+        }
+
+        for (int i = 0; i < encodingSequence.length() / 8; i++) {
+
+            if (!(storedValues > basevalue * 2)) {
+
+                if (!repeatedSequence)
+                    buffer += byteString[i];
+
+                if (i == 0) {
+                    buffer += byteString[i + 1];
+
+                    directory.put(buffer, Integer.toBinaryString(storedValues++));
+                    // output += directory.get(Integer.toBinaryString(storedValues - 1));
+                    output += extraBits + buffer.substring(0, buffer.length() - 8);
+                    buffer = "";
+                    continue;
                 }
+
+                if (i == (encodingSequence.length() / 8) - 1) {
+                    if (directory.containsKey(buffer)) {
+                        output += directory.get(buffer);
+                    } else {
+                        if (buffer.length() != 8)
+                            output += directory.get(buffer.substring(0, buffer.length() - 8)) + extraBits
+                                    + buffer.substring(buffer.length() - 9, buffer.length() - 1);
+                        else
+                            output += extraBits + buffer;
+                    }
+                    break;
+                }
+
+                if (repeatedSequence) {
+
+                    buffer += byteString[i + 1];
+
+                    if (directory.containsKey(buffer)) {
+                        continue;
+                    } else {
+
+                        directory.put(buffer, Integer.toBinaryString(storedValues++));
+                        // output += directory.get(Integer.toBinaryString(storedValues - 1));
+                        output += directory.get(buffer.substring(0, buffer.length() - 8));
+                        buffer = "";
+                        repeatedSequence = false;
+                    }
+                } else {
+
+                    buffer += byteString[i + 1];
+
+                    if (directory.containsKey(buffer)) {
+                        repeatedSequence = true;
+                        continue;
+                    } else {
+                        directory.put(buffer, Integer.toBinaryString(storedValues++));
+                        // output += directory.get(Integer.toBinaryString(storedValues - 1));
+                        output += extraBits + buffer.substring(0, buffer.length() - 8);
+                        buffer = "";
+                    }
+                }
+
+            } else {
+                System.out.println("out of space");
                 break;
             }
-
-            if (repeatedSequence) {
-                if (directory.containsKey(buffer)) {
-                    continue;
-                } else {
-                    directory.put(buffer, Integer.toBinaryString(storedValues++));
-                    // output += directory.get(Integer.toBinaryString(storedValues - 1));
-                    output += directory.get(buffer.substring(0, buffer.length() - 1))
-                            + (buffer.charAt(buffer.length() - 1) + "");
-                    buffer = "";
-                    repeatedSequence = false;
-                }
-            } else {
-                if (directory.containsKey(buffer)) {
-                    repeatedSequence = true;
-                    continue;
-                } else {
-                    directory.put(buffer, Integer.toBinaryString(storedValues++));
-                    // output += directory.get(Integer.toBinaryString(storedValues - 1));
-                    output += buffer;
-                    buffer = "";
-                }
-            }
-
         }
+        System.out.println("Memory State: " + storedValues);
         return output;
     }
 
-    public static String encode(int encodingSequence) {
-        return encode(encodingSequence + "");
+    public static String encodeInt(int encodingSequence) {
+        return encodeBinary(encodingSequence + "");
     }
+
 }
